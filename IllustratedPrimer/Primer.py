4aa6eb91a75eb4823b2d4418a0db1e3600362717
Harper Reed <harper@nata2.org>
2023-05-25 16:09:19 -0500

    updated to be more conversational

--- IllustratedPrimer/Primer.py
+++ IllustratedPrimer/Primer.py
@@ -3,11 +3,14 @@ import os
 import json
 from datetime import datetime
 import requests
+import yaml
 
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain import PromptTemplate, LLMChain
 from langchain.callbacks import get_openai_callback
+# from langchain.memory import ChatMessageHistory
+from langchain.memory import ConversationBufferMemory
 
 from homeassistant_api import Client
 
@@ -17,7 +20,6 @@ class Primer:
 
         prompt_url = os.getenv("PRIMER_PROMPT_URL") 
         self.prompt_template = self.get_prompt(prompt_url)
-   
 
         openai_model = os.getenv("OPENAI_MODEL", "gpt-3.5-turbo")
         openai_temperature = os.getenv("OPENAI_TEMPERATURE", "0")
@@ -25,17 +27,8 @@ class Primer:
         self.logger.debug(f"OpenAI Temperature: {openai_temperature}")
 
         self.chat = ChatOpenAI(model_name=openai_model, temperature=openai_temperature)
-
-    def get_hass_data(self):
-        self.logger.debug("let's get some data from hass")
-            
-        client = Client(f'{os.getenv("HASS_URL")}/api/', os.getenv("HASS_TOKEN"))
-
-        weather = client.get_entity(entity_id="weather.tomorrow_io_reedazawa_daily")
-        state = weather.get_state()
-        weather_str = f'{state.attributes["temperature"]} {state.attributes["temperature_unit"]} and {state.state}'
-
-        return weather_str
+        self.memory = ConversationBufferMemory(memory_key="chat_history")
+                      
 
     def get_prompt(self, url):
         self.logger.debug("let's get a prompt from github")
@@ -53,18 +46,26 @@ class Primer:
 
         # convert the json payload to a dict
         json_payload = json.loads(payload)
-        print(json_payload)
 
         with get_openai_callback() as cb:
-            prompt = PromptTemplate(template=self.prompt_template, input_variables=["context"])
-            # get a chat completion from the formatted messages
-            chain = LLMChain(llm=self.chat, prompt=prompt)
-            result = chain.run(context=payload)
+
+            prompt = PromptTemplate(
+                input_variables=["chat_history", "human_input"], 
+                template=self.prompt_template
+            )
+            
+            # # get a chat completion from the formatted messages
+            chain = LLMChain(llm=self.chat, prompt=prompt,   verbose=False, memory=self.memory)
+            result = chain.predict(human_input=json.dumps(json_payload, separators=(',', ':')))
+            # print(dir(self.memory))
+            if (len(self.memory.chat_memory.messages)>10):
+                self.memory.clear()
             self.logger.debug(f"OpenAI Response: {result}")
             self.logger.info(f"Total Tokens: {cb.total_tokens}")
             self.logger.info(f"Prompt Tokens: {cb.prompt_tokens}")
             self.logger.info(f"Completion Tokens: {cb.completion_tokens}")
             self.logger.info(f"Total Cost (USD): ${cb.total_cost}")
+
 
         self.logger.debug(f"OpenAI Response: {result}")
     
